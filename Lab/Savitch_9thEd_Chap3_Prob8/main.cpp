a3bd3270101177ad2debf9ac639e891a0c76d9dd
ml1150258 <mark.lehr@rcc.edu>
2015-07-02 14:07:51 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/Savitch_9thEd_Chap3_Prob8/main.cpp
+++ Lab/Savitch_9thEd_Chap3_Prob8/main.cpp
@@ -21,17 +21,20 @@ const double PI=4*atan(1.0);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    int nTerms=200000000;
+    int nTerms=20000000;
     long double apprxPI=1.0L;
+    long double tol=1e-50;
     
     //Now calculate PI with a for-loop
     for(int i=3,nTerm=1;nTerm<=nTerms;i+=4,nTerm+=2){
-        apprxPI+=(-1.0L/i+1.0L/(i+2));
+        long double del=(-1.0L/(i+0.0L)+1.0L/(i+2.0L));
+        apprxPI+=del;
+        if(del>-tol&&del<tol)break;
     }
-    apprxPI*=4;
+    apprxPI*=4.0L;
     
     //Output the results and compare
-    cout<<fixed<<showpoint<<setprecision(15);
+    cout<<fixed<<showpoint<<setprecision(20);
     cout<<"Exact Value of PI = "<<PI<<endl;
     cout<<"The approximate value of PI = "<<apprxPI<<endl;
     
