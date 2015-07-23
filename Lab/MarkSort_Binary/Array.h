1c5a7b68d029eee5db8c0ce5e8dc90bd587f4295
ml1150258 <mark.lehr@rcc.edu>
2015-07-23 12:02:07 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Lab/MarkSort_Binary/Array.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   Array.h
+ * Author: Dr. Mark E. Lehr
+ * Created on July 23, 2015, 10:54 AM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+
+struct Array{
+    int size;
+    int *data;
+};
+
+#endif	/* ARRAY_H */
+
